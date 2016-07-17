13a345b70dba9f6fca92c1dda5b92e5a9c124ad0
Darren Vong <d.vongy@gmail.com>
2016-07-17 03:38:01 +0100

    Updated footer info

--- footer.php
+++ footer.php
@@ -16,6 +16,7 @@
   <hr>
 
   <p>All rights reserved. Designed by <a target="blank" href="http://joshuahackett.com">Joshua Hackett</a>.
+  Maintained by <a href="https://github.com/darrenvong">Darren Vong</a>.
   <br>
   Want to reuse our content? <a href="http://forgetoday.com/tv/contact">Get in touch</a>.</p>
 
