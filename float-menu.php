6295314e03f36f678371b7772aa8a6931fb99325
Darren Vong <d.vongy@gmail.com>
2016-07-28 03:38:14 +0100

    Changes up to search form. Still working out way to generate menu items to adapt to this orginal WP template...

--- float-menu.php
+++ float-menu.php
@@ -1,5 +1,18 @@
 <nav id="mobile">
-
+  <!-- Likely to need refactoring later... -->
+  <div class="menu-container">
+    <ul id="mobile-primary">
+      <?php
+        //menu logic
+        // foreach ($page->menu as $menu) {
+        //   foreach ($menu->$menu_item_group as $group) {
+        //     echo "<li><a href='#'>{$group->menu_item}</a></li>";
+        //   }
+        // }
+      ?>      
+    </ul>
+  </div>
+  
   <?php
     $args = array(
     'theme_location' => 'right',
@@ -10,7 +23,7 @@
 
 
 
-  <?php get_search_form(); ?>
+  <?php include $config->paths->templates . "FTV-2016/searchform.php"; ?>
 
   <?php
     $args = array(
--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo $pages->get('template=search')->url; ?>">
 	<div>
 		<label class="screen-reader-text" for="s"><?php _x( 'Search for:', 'label' ); ?></label>
-		<input class="formSearchBoxJS" type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+		<input class="formSearchBoxJS" type="text" value="<?php echo $sanitizer->entities($input->whitelist('s')); ?>" name="s" id="s" />
 		// <input type="submit" class="search-submit"
-		 value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>"/>
+		 value="<?php echo $sanitizer->text('Search'); ?>"/>
 	</div>
 </form>
