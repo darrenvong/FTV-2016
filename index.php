741fc0f143b644ecd03cd3ae171b7e5375bb30ba
Darren Vong <d.vongy@gmail.com>
2016-08-07 03:16:30 +0100

    Cleaned up redundant files as a result of the conversion

--- index.php
+++
@@ -1,3 +0,0 @@
-<?php include_once $config->paths->templates. "FTV-2016/header.php"; ?>
-
-<?php include_once $config->paths->templates . "FTV-2016/footer.php"; ?>
--- single.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if (in_category('committee-blog')) {
-    get_template_part('single-blog');
-  } else if (in_category('upcoming-live')) {
-    get_template_part('single-live');
-  }else{
-    get_template_part('single-vid');
-  };
-?>
