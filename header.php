120e13257ad92bcebd439d553ba60364b594099d
Darren Vong <d.vongy@gmail.com>
2016-12-19 04:10:18 +0000

    Added fb:pages meta in preparation for Facebook Instant Articles

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title('|','true','right'); ?><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
+    <meta property="fb:pages" content="232123353469992" />
     <?php wp_head(); ?>
 </head>
 <body id="top">
